c96794dacf7634f48d658edb7ac460bb0506e2ae
Alexander Burkovskiy <byrkovskiy@gmail.com>
2014-05-03 20:38:40 +0300

    Patterns

---
+++ JAVA/Patterns/Singleton1.java
@@ -0,0 +1,18 @@
+package patterns.singleton;
+
+public class Singleton1 {
+
+	private static Singleton1 singleton;
+
+	private Singleton1() {
+
+	}
+
+	public static Singleton1 getInstance() {
+		if (singleton == null) {
+			singleton = new Singleton1();
+		}
+		return singleton;
+	}
+
+}
